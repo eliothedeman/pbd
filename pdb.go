782fb0ba712c248c4a39ba359b34a6da7ffa14f4
Eliot Hedeman <ehedeman@barracuda.com>
2014-12-26 16:29:37 -0500

    added stubbed values

---
+++ pdb.go
@@ -0,0 +1,34 @@
+package gopdb
+
+// PDB
+type PDB struct {
+}
+
+// TODO add full structures for each of these
+type Atom string
+type Author string
+type Compound string
+type Connection string
+type Crystal string
+type DatabaseReference string
+type ExperimentalData string
+type Formula string
+type Header string
+type Helix string
+type HeteroAtom string
+type HeteroAtomName string
+type Journal string
+type Keywords string
+type Link string
+type Master string
+type ModifiedResidue string
+type Orign string
+type Remark string
+type ResidueSequence string
+type Revision string
+type Scale strin
+type Sheet string
+type Site string
+type Source string
+type SSBond string
+type Title string
